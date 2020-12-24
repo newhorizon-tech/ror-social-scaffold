0739af351b1c27314e7fa7bea0088f578adf618c
newhorizon-tech <alaukikinbox@gmail.com>
2020-12-24 21:48:18 +0530

    Add notifications in the data model

--- app/models/friendship_request.rb
+++ app/models/friendship_request.rb
@@ -1,4 +1,15 @@
 class FriendshipRequest < ApplicationRecord
     belongs_to :request_sender, class_name: 'User'
     belongs_to :request_receiver, class_name: 'User'
+
+    def accept_request(sender)
+      self.status = 'accepted'
+    end
+
+    def reject_request
+      self.status = 'rejected'
+    end
+
+    scope :pending, -> { where(status: 'pending')}
+    scope :accepted, -> { where(status: 'accepted')}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,17 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :sent_requests, class_name: 'FriendshipRequest', foreign_key: 'request_sender_id'
-  has_many :receiver_requests, class_name: 'FriendshipRequest', foreign_key: 'request_receiver_id'
-end
+  has_many :request_senders, class_name: 'FriendshipRequest', foreign_key: 'request_sender_id'
+  has_many :request_receivers, class_name: 'FriendshipRequest', foreign_key: 'request_receiver_id'
 
+  def notifications
+     notification_list = FriendshipRequest.pending.where(request_receiver_id: self.id)
+  end
+
+  def accept_request(sender)
+    fr = FriendshipRequest.pending.where(request_receiver_id: self.id, request_sender_id: sender.id)
+    FriendshipRequest.accept_request
+  end
+
+
+end
