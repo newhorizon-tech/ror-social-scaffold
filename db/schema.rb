c5521d8cad037fbc50116cebf9392343f1979842
Tendai Nyandoro <tnyandoro@gmail.com>
2020-12-24 10:40:58 +0200

    Improve friendship request model

--- db/schema.rb
+++ db/schema.rb
@@ -26,8 +26,12 @@ ActiveRecord::Schema.define(version: 2020_12_23_152602) do
   end
 
   create_table "friendship_requests", force: :cascade do |t|
+    t.bigint "request_receiver_id"
+    t.bigint "request_sender_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["request_receiver_id"], name: "index_friendship_requests_on_request_receiver_id"
+    t.index ["request_sender_id"], name: "index_friendship_requests_on_request_sender_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -59,4 +63,6 @@ ActiveRecord::Schema.define(version: 2020_12_23_152602) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "friendship_requests", "users", column: "request_receiver_id"
+  add_foreign_key "friendship_requests", "users", column: "request_sender_id"
 end
