6e3001493d84e922c4dcd488ccb3ff8bf06c360b
Tendai Nyandoro <tendai1@hotmail.com>
2020-12-30 17:34:06 +0200

    Add Rspec test on post model

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ end
 
 group :development, :test do
   gem 'rspec-rails'
+  gem 'factory_bot_rails'
 end
 
 group :development do
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  context 'Input Validation' do  # (almost) plain English
+    let(:test_user){User.create(name: "TestUser", email: "testuser@email.com",
+                            password: "pineapple", password_confirmation: "pineapple")}
     it 'cannot create post without any user' do
       expect {
         Post.create!(content: "Content")
       }.to raise_error(ActiveRecord::RecordInvalid)
     end
-  end
+    it 'cannot create post without any content' do
+      test_post = test_user.posts.create(content: "")
+      expect(test_post).to_not be_valid
+    end
+    it 'create a valid post with content' do
+      test_post = test_user.posts.create(content: "Its a post")
+      expect(test_post).to be_valid
+    end
 end
